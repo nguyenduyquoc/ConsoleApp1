71b2a17651a81818bd5030ae170e918cabc6fe45
Nguyen Duy Quoc <nguyenduyquoc129829042001@gmail.com>
2023-01-04 20:54:17 +0700

    cmt

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1.Session1;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //int x = 10;
+        //double y = 3.14;
+        //Console.WriteLine("Hello world: " + x);
+        //string s = "T2204M";
+
+        //int[] ns = new int[3];
+        //ns[0] = 15;
+        //ns[1] = 20;
+        //ns[2] = 25;
+
+        //foreach (int n in ns)
+        //{
+        //    // n
+        //}
+
+        //for (int i = 0; i < ns.Length; i++)
+        //{
+        //    // n[i]
+        //}
+
+        ////INPUT AND OUTPUT
+        //string input = Console.ReadLine(); // scan
+        //int num = Convert.ToInt32(input);
+        //Console.WriteLine("num = " + num);
+
+        ////CLASS AND OBJECT
+        Human h = new Human();
+        h.name = "Hoang Duy Quoc";
+        h.Run();
+
+
+        // ---------------- TEST ASSIGNMENT 1 ----------------
+
+
+    }
+}
---
+++ ConsoleApp1/Session1/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Session1
+{
+    internal class Human
+    {
+        public string name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine("Eating.....");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(this.name + "is running...");
+            
+        }
+
+       
+    }
+}
